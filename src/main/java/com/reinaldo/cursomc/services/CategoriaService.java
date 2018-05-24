5466079e6f5be1d683424fce951eb13b42ccd0c8
Reinaldo Caetano <rei_caetano@hotmail.com>
2018-05-24 15:43:14 -0300

    Criando relacionamento produto categoria

--- src/main/java/com/reinaldo/cursomc/services/CategoriaService.java
+++ src/main/java/com/reinaldo/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.reinaldo.cursomc.domain.Categoria;
 import com.reinaldo.cursomc.repositories.CategoriaRepository;
+import com.reinaldo.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		//return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
 }
